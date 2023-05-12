a363220adae94d9ab096e4746aa667fb7c025db3
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2023-05-13 05:25:45 +0600

    Add LoginDTO and Login end point

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -30,6 +30,21 @@ namespace API.Controllers
             await _context.SaveChangesAsync();
             return user;
         }
+        [HttpPost("login")] // POST: api/account/login
+        public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDTO.Username);
+            if (user == null) return Unauthorized("invalid user");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+            for(int i = 0; i < computedHash.Length; i++)
+            {
+                if (user.PasswordHash[i] != computedHash[i])
+                    return Unauthorized("invalid password");
+            }
+            return user;
+        }
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
