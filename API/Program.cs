c7ac8cfa8891c441b86a5528e04fd5065876b47b
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2023-05-12 08:27:48 +0600

    Fetch data from API to UI

--- API/Program.cs
+++ API/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
 builder.Services.AddDbContext<ApplicationDbContext>(m => m.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
@@ -13,10 +14,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(m => m.UseSqlServer(
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+//app.UseHttpsRedirection();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
