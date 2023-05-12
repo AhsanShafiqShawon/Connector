7490d6d26c1a1ef5e176b3faed4196bbeb8bdf76
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2023-05-13 02:24:12 +0600

    Add AccountController

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly ApplicationDbContext _context;
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost("register")] // POST: api/account/register
+        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        {
+            using var hmac = new HMACSHA512();
+            var user = new AppUser
+            {
+                UserName = username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+    }
+}
