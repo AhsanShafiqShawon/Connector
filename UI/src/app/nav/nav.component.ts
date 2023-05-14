95423a582e9d7773497eb5377d7a251514d4610f
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2023-05-14 20:13:24 +0600

    Use conditionals to show and remove content

--- UI/src/app/nav/nav.component.ts
+++ UI/src/app/nav/nav.component.ts
@@ -10,15 +10,19 @@ export class NavComponent {
   model: any = {};
   loggedIn = false;
 
-  constructor(private accountService: AccountService){}
-  
+  constructor(private accountService: AccountService) {}
+
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
+      next: (response) => {
         console.log(response);
         this.loggedIn = true;
       },
-      error: error => console.log(error)
+      error: (error) => console.log(error),
     });
+  }
+
+  logout() {
+    this.loggedIn = false;
   }
 }
