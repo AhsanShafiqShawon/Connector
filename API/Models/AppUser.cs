349111518f876f4dcb17ba69bac262d10c6e43d8
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2023-05-21 18:41:40 +0600

    Add migrations

--- API/Models/AppUser.cs
+++ API/Models/AppUser.cs
@@ -8,7 +8,7 @@ namespace API.Models
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-        public DateOnly DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
