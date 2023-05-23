9a64a5f5aec2db00e4e24ba745d6a888c6426c93
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2023-05-23 09:04:14 +0600

    Use AutoMapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 ﻿using API.Data;
+using API.DTOs;
 using API.Interfaces;
 using API.Models;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,19 +13,25 @@ namespace API.Controllers
     public class UsersController : BaseApiController
     {
         private readonly IUserRepository _userRepository;
-        public UsersController(IUserRepository userRepository)
+        private readonly IMapper _mapper;
+
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
             _userRepository = userRepository;
+            _mapper = mapper;
         }
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDTO>>> GetUsers()
         {
-            return Ok(await _userRepository.GetUsersAsync());
+            var users = _userRepository.GetUsersAsync();
+            var usersToReturn = _mapper.Map<IEnumerable<MemberDTO>>(users);
+            return Ok(usersToReturn);
         }
         [HttpGet("{username}")]
-        public async Task<ActionResult<AppUser>> User(string username)
+        public async Task<ActionResult<MemberDTO>> User(string username)
         {
-            return await _userRepository.GetUserByUsernameAsync(username);
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+            return _mapper.Map<MemberDTO>(user);
         }
     }
 }
--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -1,6 +1,15 @@
-﻿namespace API.Helpers
+﻿using API.DTOs;
+using API.Models;
+using AutoMapper;
+
+namespace API.Helpers
 {
-    public class AutoMapperProfiles
+    public class AutoMapperProfiles : Profile
     {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDTO>();
+            CreateMap<Photo, PhotoDTO>();
+        }
     }
 }
