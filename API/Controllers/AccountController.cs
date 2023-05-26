4a0a766a5cbda69cd76d8517eb6dd1f9ca112bb5
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2023-05-26 15:34:16 +0600

    Add main photo to the nav bar

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -40,7 +40,9 @@ namespace API.Controllers
         [HttpPost("login")] // POST: api/account/login
         public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDTO.Username);
+            var user = await _context.Users
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync(x => x.UserName == loginDTO.Username);
             if (user == null) return Unauthorized("invalid username");
 
             using var hmac = new HMACSHA512(user.PasswordSalt);
@@ -53,7 +55,8 @@ namespace API.Controllers
             return new UserDTO
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
         private async Task<bool> UserExists(string username)
